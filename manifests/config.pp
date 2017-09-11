9b8a2915702ee1c12c19b2e2a56a098a9366a6f2
Tommy Speigner <tspeigner@gmail.com>
2017-09-11 16:00:51 -0700

    added file line to edit entry

--- manifests/config.pp
+++ manifests/config.pp
@@ -6,5 +6,11 @@
 #
 # @example
 #   include win_apache::config
-class win_apache::config {
+class win_apache::config (
+  $conffile = 'C:\Users\Administrator\AppData\Roaming\Apache24\conf\httpd.conf'
+){
+  file_line { 'sudo_rule':
+    path => "${conffile}",
+    line => 'Listen 80',
+  }
 }
--- manifests/install.pp
+++ manifests/install.pp
@@ -8,12 +8,9 @@
 #   include win_apache::install
 class win_apache::install (
   $apache_version   = installed, #use installed, absent for uninstalled, or version number
-  $install_location = 'c:\\tools\\apache', #change to whichever location you desire
-  $port             = '80', #defaults to 80
 ) {
     package { 'apache-httpd':
       ensure   => $apache_version,
       provider => chocolatey,
-      install_options => ['-ia', '"', '/installLocation="c:\\tools\\apache", '"'']
     }
 }
