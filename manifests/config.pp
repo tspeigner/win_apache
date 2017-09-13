9319fef88597856f5966c7c69f2021c41ce3435d
Tommy Speigner <tspeigner@gmail.com>
2017-09-13 12:16:41 -0700

    adding a new path for conf file

--- manifests/config.pp
+++ manifests/config.pp
@@ -7,7 +7,7 @@
 # @example
 #   include win_apache::config
 class win_apache::config (
-  $conffile = 'C:\Users\Administrator\AppData\Roaming\Apache24\conf\httpd.conf'
+  $conffile = 'c:\tools\apache24\conf\httpd.conf'
 ){
   file_line { 'sudo_rule':
     path => "${conffile}",
