3b154f8dff85b0b458533cf343788cb601ac1d70
Tommy Speigner <tspeigner@gmail.com>
2017-09-13 11:48:18 -0700

    trying the install location again based on input from choco community

--- manifests/install.pp
+++ manifests/install.pp
@@ -8,9 +8,12 @@
 #   include win_apache::install
 class win_apache::install (
   $apache_version   = installed, #use installed, absent for uninstalled, or version number
+  $install_location = 'c:\tools\apache',
+
 ) {
     package { 'apache-httpd':
-      ensure   => $apache_version,
-      provider => chocolatey,
+      ensure          => $apache_version,
+      provider        => chocolatey,
+      install_options => ['--params', "'/installLocation:""${install_location}""'"]
     }
 }
