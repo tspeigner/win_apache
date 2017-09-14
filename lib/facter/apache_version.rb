fc2f72796e428049cd179ee149e0f248e51d9f6c
Tommy Speigner <tspeigner@gmail.com>
2017-09-14 08:27:07 -0700

    changing index

--- lib/facter/apache_version.rb
+++ lib/facter/apache_version.rb
@@ -2,6 +2,6 @@ require 'yaml'
 
 Facter.add(:apache_version) do
   setcode do
-    YAML.safe_load(Facter::Util::Resolution.exec('puppet resource package apache-httpd')['package']['apache-httpd'])
+    YAML.safe_load(Facter::Util::Resolution.exec('puppet resource package apache-httpd')['package'])
   end
 end
