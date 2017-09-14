13f2d530443c8b2d8d72a56818d26d290101e646
Tommy Speigner <tspeigner@gmail.com>
2017-09-14 10:36:11 -0700

    variable change

--- manifests/config.pp
+++ manifests/config.pp
@@ -9,7 +9,7 @@
 class win_apache::config (
   $conffile = 'c:\tools\apache24\conf\httpd.conf'
 ) {
-  file { "${win_apache::conffile}":
+  file { $win_apache::conffile:
     ensure  => file,
     content => epp('win_apache/httpd.conf.epp'), #basic template file
   }
