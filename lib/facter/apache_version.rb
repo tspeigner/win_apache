ad5994965d04e8d3a274453954407f041e95bd56
Tommy Speigner <tspeigner@gmail.com>
2017-09-14 08:45:22 -0700

    added all things back

--- lib/facter/apache_version.rb
+++ lib/facter/apache_version.rb
@@ -2,6 +2,7 @@ require 'yaml'
 
 Facter.add(:apache_version) do
   setcode do
-    YAML.safe_load(Facter::Util::Resolution.exec('puppet resource package apache-httpd --to_yaml')['package'])
+    YAML.safe_load(Facter::Util::Resolution.exec('puppet resource package apache-httpd --to_yaml')['package']['apache-httpd']['ensure'])
+    #YAML.load(test)['package']['tar']['ensure']
   end
 end
