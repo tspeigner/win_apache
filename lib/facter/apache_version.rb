c3b09a9ed2ee597a08b611c56791f4883f145cc9
Tommy Speigner <tspeigner@gmail.com>
2017-09-27 15:12:18 -0700

    moving archive out

--- lib/facter/apache_version.rb
+++
@@ -1,9 +0,0 @@
-Facter.add(:apache_version) do
-  confine :kernel => 'windows'
-  setcode do
-    # Using choco list on the package namage, then splitting the results (on space)
-    # then getting the 4 item.  This might change later if choco list has a different output
-    # but this works for now.
-    Facter::Util::Resolution.exec('choco list apache-httpd').split[3]
-  end
-end
